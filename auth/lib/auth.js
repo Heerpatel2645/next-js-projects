61aacdba96d98ec1e3ab81237cf900c4ef7cc2bc
Heerpatel2645 <heerpatelhmt@gmail.com>
2026-02-07 22:01:20 +0530

    Add Next.js practice projects

---
+++ auth/lib/auth.js
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "./db";
+
+export const auth = betterAuth({
+  secret: process.env.SECRET,
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    // github: {
+    //   clientId: process.env.GITHUB_CLIENT_ID,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    // },
+  },
+});
---
+++ auth/lib/db.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "./generated/prisma/client"
+
+const globalForPrisma = globalThis 
+
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+})
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
